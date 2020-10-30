db0cbf1e6665846a97073c7804b421634cdf00c5
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2020-10-31 01:18:02 +0800

    added full crud in orderdetail-items

--- app/controllers/orderDetail.controllers.js
+++ app/controllers/orderDetail.controllers.js
@@ -1,5 +1,7 @@
 const OrderDetail = require("../models/orderDetail.model");
+const Order = require("../models/order.model");
 const mongoose = require("mongoose");
+mongoose.set("useFindAndModify", false);
 
 exports.create = async (req, res) => {
 	// Create Order
@@ -26,8 +28,9 @@ exports.findAll = async (req, res) => {
 	try {
 		const data = await OrderDetail.find()
 			// .select("order_date customer")
-			.populate("product")
-			.populate("order");
+			// .populate("product")
+			// .populate("order")
+			.where("order", req.params.orderID);
 
 		res.send(data);
 	} catch (error) {
@@ -38,15 +41,15 @@ exports.findAll = async (req, res) => {
 	}
 };
 
-//ALL THE CODES BELOW WERE NOT USE
 exports.findOne = async (req, res) => {
 	try {
-		const data = await OrderDetail.findById(req.params.orderDetailID).populate(
-			"customer"
-		);
+		const data = await OrderDetail.findById(req.params.orderDetailID)
+			.populate("order")
+			.populate("product")
+			.where("order", req.params.orderID);
 		if (!data) {
 			return res.status(404).send({
-				message: "record not found with id " + req.params.OrderDetailID,
+				message: "record not found with id " + req.params.orderDetailID,
 			});
 		}
 		res.send(data);
@@ -54,6 +57,56 @@ exports.findOne = async (req, res) => {
 		res.status(500).send({
 			message:
 				error.message || "Some error occurred while creating order data.",
+		});
+	}
+};
+
+exports.update = async (req, res) => {
+	const option = { new: true };
+	const reqBody = {
+		quantity: req.body.quantity,
+		price: req.body.price,
+	};
+
+	try {
+		const data = await OrderDetail.findByIdAndUpdate(
+			req.params.orderDetailID,
+			reqBody,
+			option
+		);
+		if (!data) {
+			return res.status(404).send({
+				message: "record not Record with id " + req.params.orderDetailID,
+			});
+		}
+
+		res.send(data);
+	} catch (error) {
+		res.status(500).send({
+			message:
+				error.message || "Some error occurred while creating Record data.",
+		});
+	}
+};
+
+exports.delete = async (req, res) => {
+	try {
+		const data = await OrderDetail.findByIdAndRemove(req.params.orderDetailID);
+		if (!data) {
+			return res.status(404).send({
+				message: "Record not found with id " + req.params.orderDetailID,
+			});
+		}
+
+		res.send(data);
+	} catch (err) {
+		if (err.kind === "ObjectId" || err.name === "NotFound") {
+			return res.status(404).send({
+				message: "Record not found with id " + req.params.orderDetailID,
+			});
+		}
+		return res.status(500).send({
+			message: "Could not delete Record with id " + req.params.orderDetailID,
 		});
 	}
 };
